b058f7d60d078068b054e37314d81152f87c0cf7
Nathan <nathan@nathans-mac-pro.fios-router.home>
2018-02-05 11:18:58 -0800

    challenges next, page 194

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -17,7 +17,9 @@ class ItemsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
         let item = itemStore.allItems[indexPath.row]
         
@@ -25,4 +27,23 @@ class ItemsViewController: UITableViewController {
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         return cell
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
